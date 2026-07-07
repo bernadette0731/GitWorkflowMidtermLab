3654cf3b1ec1a74450f28a92ae2e426ca55c889f
bernadette0731 <tariaobernadette@gmail.com>
2026-07-07 10:03:08 +0800

    Defined variables to be used in script.js file

---
+++ script.js
@@ -0,0 +1,3 @@
+const modalOverlay = document.getElementById('modalOverlay');
+const modalTriggers = document.querySelectorAll('.toggle-modal');
+const themeBtn = document.getElementById('themeBtn');
