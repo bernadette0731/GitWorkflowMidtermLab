ebe964f275a56fa5c6f0551a5568279a70311be0
bernadette0731 <tariaobernadette@gmail.com>
2026-07-07 10:16:11 +0800

    Modified the script.js file and added functions for toggling the modals and theme

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const modalOverlay = document.getElementById('modalOverlay');
 const modalTriggers = document.querySelectorAll('.toggle-modal');
 const themeBtn = document.getElementById('themeBtn');
+
+// Modal toggle
+modalTriggers.forEach(btn => btn.addEventListener('click', () => modalOverlay.classList.add('active')));
+document.getElementById('closeModal').addEventListener('click', () => modalOverlay.classList.remove('active'));
+
+// Theme Toggle
+themeBtn.addEventListener('click', () => {
+    document.documentElement.toggleAttribute('data-theme');
+});
